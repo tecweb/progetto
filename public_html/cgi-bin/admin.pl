8f345783ebbe5e05eac194c34e0473613a3e8605
Mirco Geremia <crittico@gmail.com>
2009-03-09 15:38:07 +0100

    admin: aggiungi news funzionante

--- public_html/cgi-bin/admin.pl
+++ public_html/cgi-bin/admin.pl
@@ -10,14 +10,14 @@ use CGI::Session;
 do "base.pl";
 
 sub not_admin {
-	 print <<'EOF';
+  print <<'EOF';
 	 <h1>Accesso negato!</h1>
 	 <p>Eseguire il login come amministratore per accedere a questa pagina.</p>
 EOF
 }
 
-my sub admin_func{
-	 print <<'EOF';
+sub admin_func{
+  print <<'EOF';
 	 <div id="admin_func">
 		  <a href="read_pro.pl">Leggi proposte</a><br/>
 		  <a href="add_tem.pl">Aggiungi tematica</a><br/>
@@ -29,10 +29,10 @@ EOF
 }
 
 print_doc_start("Amministrazione");
-if (get_user_name() eq 'Utente'){
-	 admin_func();
+if (get_user_name() eq 'admin30'){
+  admin_func();
 }
 else {
-	 not_admin();
+  not_admin();
 }
 print_doc_end();
