e657cc55c586c9d22082e32fc6f8385252384b98
Alessandro Renosto <realex@fenomenALE.(none)>
2009-03-11 00:27:30 +0100

    inizio news

--- public_html/cgi-bin/home.pl
+++ public_html/cgi-bin/home.pl
@@ -4,6 +4,17 @@ use strict;
 use warnings;
 do "base.pl";
 
+use lib 'mymodules/share/perl/5.8/';
+use CGI qw( :standard );
+use XML::DOM;
+
 print_doc_start('Home', 'Homepage');
-print "<p> Qui va il testo </p>\n";
+
+my $xml_file = get_root() . '/eventi/index.xml';
+my $parser = XML::DOM::Parser->new;
+my $doc = $parser->parsefile($xml_file);
+
+print <<'EOF';
+    <h3> News </h3>
+EOF
 print_doc_end();
