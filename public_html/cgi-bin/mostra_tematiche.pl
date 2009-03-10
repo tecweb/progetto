2b31a8fc198ac36393ab429ae530cf5c8f8d1f1d
Stefano Dissegna <stefano.dissegna@gmail.com>
2009-03-10 14:11:46 +0100

    numerazione tematiche

--- public_html/cgi-bin/mostra_tematiche.pl
+++ public_html/cgi-bin/mostra_tematiche.pl
@@ -33,9 +33,12 @@ print "<h1 id=\"tem_list_title\"> Da $from a $to </h1>";
 
 print '<ol id="tem_list">';
 
+my $i = $from;
 foreach (@tem) {
   my $d = tem_descr($_);
-  print "<li> <a href=\"/cgi-bin/tematica.pl?ref=$_\"> $_ </a> <p> $d </p> </li>";
+  ## numerazione a mano per non ripartire sempre da 0
+  print "<li> $i. <a href=\"/cgi-bin/tematica.pl?ref=$_\"> $_ </a> <p> $d </p> </li>";
+  $i++;
 }
 
 print '</ol>';
