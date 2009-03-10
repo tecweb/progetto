abb83c426dad319704d92679af2c5fef1dea4f07
Stefano Dissegna <stefano.dissegna@gmail.com>
2009-03-10 13:36:43 +0100

    elenco tematiche con descrizioni

--- public_html/cgi-bin/mostra_tematiche.pl
+++ public_html/cgi-bin/mostra_tematiche.pl
@@ -5,12 +5,21 @@ use warnings;
 
 use lib 'mymodules/share/perl/5.8/';
 use CGI qw( :standard );
+use XML::XPath;
 
 do "base.pl";
 
 my $from = param('from') || 0;
 $from = 0 if $from < 0;
 my $to = $from + 9;
+
+sub tem_descr {
+  my $xp;
+  eval {
+    $xp = XML::XPath->new(filename => get_tem_dir() . $_ . '/index.xml');
+  } or return "";
+  return $xp->findvalue('/tematica/descrizione/text()')->value() || "";
+}
 
 print_doc_start("Tematiche");
 
@@ -23,7 +32,12 @@ $to = $n if $to > $n;
 print "<h1 id=\"tem_list_title\"> Da $from a $to </h1>";
 
 print '<ol id="tem_list">';
-print "<li> <a href=\"/cgi-bin/tematica.pl?ref=$_\"> $_ </a> </li>" foreach (@tem);
+
+foreach (@tem) {
+  my $d = tem_descr($_);
+  print "<li> <a href=\"/cgi-bin/tematica.pl?ref=$_\"> $_ </a> <p> $d </p> </li>";
+}
+
 print '</ol>';
 
 if ($from > 0) {
