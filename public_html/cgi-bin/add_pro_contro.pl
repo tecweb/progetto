8186516732fc101ecfe1ba95fa533002543ba0cb
Mirco Geremia <crittico@gmail.com>
2009-03-12 21:09:55 +0100

    admin: tematiche work in progress

---
+++ public_html/cgi-bin/add_pro_contro.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 'mymodules/share/perl/5.8/';
+use CGI qw( :standard );
+use CGI::Session;
+use XML::DOM;
+use File::Path;
+
+do "base.pl";
+
+my $root = get_root();
+
+our $nome = param('name');
+my $sol = param('sol');
+my $title = param('title');
+
+sub not_admin {
+  print <<'EOF';
+	 <h1>Accesso negato!</h1>
+	 <p>Eseguire il login come amministratore per accedere a questa pagina.</p>
+EOF
+}
+
+sub add(){
+  open (FILE, ">$root/tematiche/$nome/$title.xml");
+  print FILE "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+  print FILE "<soluzione xmlns:xs=\"http://www.w3.org/2001/XMLSchema-instance\" xs:schemaLocation=\"../../../xml/schema/tematica.xsd\">\n";
+  print FILE "</soluzione>";
+  close FILE;
+
+  my $parser = new XML::DOM::Parser;
+  my $dom = $parser->parsefile ("$root/tematiche/$nome/$title.xml");
+  my $sl = $dom->getDocumentElement();
+
+  my $prp = $dom->createElement("proposta");
+  $prp->addText($sol);
+  $sl->appendChild($prp);
+
+  $dom->printToFile("$root/tematiche/$nome/$title.xml");
+}
+
+sub form(){
+	 print<<'EOF';
+    <form action="/cgi-bin/add_pro_contro.pl" method="POST">
+	 <fieldset>
+	 <legend>Aggiungi punti a favore</legend>
+	 <p>
+	   <label for="Pro">Pro: </label><br/>
+	   <textarea cols="50" rows="5" name="pro"></textarea>
+	 </p>
+    <p> <input class="ok" type="submit" value="Continua"/></p>
+	 </fieldset>
+    </form>
+EOF
+}
+
+print_doc_start("Aggiungi pro");
+if (get_user_name() eq 'admin'){
+  if (!(-e "$root/tematiche/$nome/$title.xml") && $title){
+	 add();
+  }
+  form();
+}
+else {
+  not_admin();
+}
+print_doc_end();
