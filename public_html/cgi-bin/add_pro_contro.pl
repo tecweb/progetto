ed15bd99af44a240582bd785c5065a2bd5de5a29
Mirco Geremia <crittico@gmail.com>
2009-03-12 23:24:51 +0100

    admin: aggiunta tematica completata

--- public_html/cgi-bin/add_pro_contro.pl
+++ public_html/cgi-bin/add_pro_contro.pl
@@ -13,9 +13,17 @@ do "base.pl";
 
 my $root = get_root();
 
-our $nome = param('name');
 my $sol = param('sol');
 my $title = param('title');
+my $pro = param('pro');
+
+my $s = CGI::Session->load();
+my $nome = $s->param('name');
+$s->flush();
+$s = new CGI::Session();
+$s->param('name', $nome);
+$s->param('ttl', $title);
+$s->flush();
 
 sub not_admin {
   print <<'EOF';
@@ -24,7 +32,7 @@ sub not_admin {
 EOF
 }
 
-sub add(){
+sub create {
   open (FILE, ">$root/tematiche/$nome/$title.xml");
   print FILE "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
   print FILE "<soluzione xmlns:xs=\"http://www.w3.org/2001/XMLSchema-instance\" xs:schemaLocation=\"../../../xml/schema/tematica.xsd\">\n";
@@ -42,25 +50,29 @@ sub add(){
   $dom->printToFile("$root/tematiche/$nome/$title.xml");
 }
 
-sub form(){
+sub form {
 	 print<<'EOF';
-    <form action="/cgi-bin/add_pro_contro.pl" method="POST">
+    <form action="/cgi-bin/add_dom.pl" method="POST">
 	 <fieldset>
-	 <legend>Aggiungi punti a favore</legend>
-	 <p>
-	   <label for="Pro">Pro: </label><br/>
-	   <textarea cols="50" rows="5" name="pro"></textarea>
-	 </p>
+	 <legend>Aggiungi pro e contro</legend>
+	 <p> <label for="Pro">Pro: </label></p>
+	 <p><input type="text" size="50" name="pro1"/></p>
+	 <p><input type="text" size="50" name="pro2"/></p>
+	 <p><input type="text" size="50" name="pro3"/></p>
+	 <p> <label for="Contro">Contro: </label></p>
+	 <p><input type="text" size="50" name="con1"/></p>
+	 <p><input type="text" size="50" name="con2"/></p>
+	 <p><input type="text" size="50" name="con3"/></p>
     <p> <input class="ok" type="submit" value="Continua"/></p>
 	 </fieldset>
     </form>
 EOF
 }
 
-print_doc_start("Aggiungi pro");
+print_doc_start("Aggiungi pro e contro");
 if (get_user_name() eq 'admin'){
   if (!(-e "$root/tematiche/$nome/$title.xml") && $title){
-	 add();
+	 create();
   }
   form();
 }
