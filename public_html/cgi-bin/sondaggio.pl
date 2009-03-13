e277795e960bd6ebd14afd589d4fddc5cbdd18f8
Alessandro Renosto <realex@fenomenALE.(none)>
2009-03-13 16:12:16 +0100

    ordine opzioni

--- public_html/cgi-bin/sondaggio.pl
+++ public_html/cgi-bin/sondaggio.pl
@@ -9,8 +9,6 @@ do 'base.pl';
 
 use CGI qw( :standard);
 use XML::DOM;
-use XML::XPath;
-use XML::XPath::XMLParser;
 
 my $tem = param('ref') || 0; 		#directory della soluzione
 my $tem_dir = get_tem_dir() . $tem;		#directory della tematica
@@ -24,59 +22,22 @@ my $text = $document->getElementsByTagName ("testo");
 
 my $desc = $document->getElementsByTagName ("descrizione");
 
-#sub print_proposta {
-#	my $xp;	
-#	eval {
-#     $xp = XML::XPath->new(filename => $_[0]);
-#   } or print "";
-#	my $prop = $xp->findvalue('/soluzione/domanda/testo/text()')->value() || "";
-#	print "<h4 class=\"proposta\"> $prop </h4>\n";
-#	#lista dei pro	
-#	my $pros = $xp->find('soluzione/domanda/opzione/descrizione/text()');
-#	#lista dei contro
-#	my $cons = $xp->find('soluzione/contro/text()');
-#	print <<'EOF';	
-#<dl>
-#<dt> Pro </dt>				
-#EOF
-#	foreach my $pro ($pros->get_nodelist) {
-#		print "<dd>",XML::XPath::XMLParser::as_string($pro),"</dd>\n";
-#	}
-#	print "<dt> Contro </dt>\n";
-#	foreach my $con ($cons->get_nodelist) {
-#		print "<dd>",XML::XPath::XMLParser::as_string($con),"</dd>\n";
-#	}
-#	print "</dl>\n";
-#}
-
-#sub print_proposte {
-#	my @files = glob "$tem_dir/";
-#	foreach my $file (@files) {
-#		my $filename = fileparse($file);		
-#		#controllo quale file sto per andare ad aprire		
-#		if($filename ne "index.xml")
-#		{	print_proposta($file); 
-#			print "<span> [<a href=\"/cgi-bin/sondaggio.pl?ref=$file\"> Vai all'approfondimento </a>] </span>";
-#			print "<span> [<a href=\"/cgi-bin/commenti.pl?ref=$tem_dir\"> Vai ai commenti </a>] </span>";
-#		}
-#	}
-#}
-
 print_doc_start("Tematica","Tematica $tem","discussione","tematiche",$tem);
 
-print	"<p> $proposta </p>\n";
+print	"<h4> $proposta </h4>\n";
 
-for (my $i = 0; $i < $text->getLength(); $i++){
+for (my $i = 0; $i < $text->getLength(); $i++)
+{
 	 my $value = $text->item($i)->getFirstChild()->getData();
 	 print
-      "<p>$value</p>"
+      "<p>$value</p>";
+	for (my $j = $i*3; $j < $i*3+3; $j++)
+	{
+		my $value2 = $desc->item($j)->getLastChild()->getData();
+		print
+    	"<p>$value2</p>";
   }
-
-for (my $i = 0; $i < $desc->getLength(); $i++){
-	 my $value = $desc->item($i)->getFirstChild()->getData();
-	 print
-      "<p>$value</p>"
-  }
+}
 
 print_proposte();
 
