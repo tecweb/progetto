112c1a9693c5c0161b2fa588bab43c0ed2e2d3f0
Stefano Dissegna <stefano.dissegna@gmail.com>
2009-03-14 10:32:10 +0100

    numerazione in mostra_tematiche.pl parte da 1

--- public_html/cgi-bin/mostra_tematiche.pl
+++ public_html/cgi-bin/mostra_tematiche.pl
@@ -9,8 +9,8 @@ use XML::XPath;
 
 do "base.pl";
 
-my $from = param('from') || 0;
-$from = 0 if $from < 0;
+my $from = param('from') || 1;
+$from = 1 if $from < 1;
 my $to = $from + 9;
 
 sub tem_descr {
@@ -26,10 +26,10 @@ sub tem_descr {
 print_doc_start("Tematiche", "visualizzazione di tutte le tematiche, ordinate per data", "tematiche");
 
 my @tem = map { basename($_) } get_ordered_tem();
-my $n = $#tem;
+my $n = $#tem + 1;
 $from = $n if $from > $n;
 $to = $n if $to > $n;
-@tem = @tem[$from .. $to];
+@tem = @tem[$from-1 .. $to-1];
 
 print "<h1 id=\"tem_list_title\"> Da $from a $to </h1>";
 
@@ -38,14 +38,14 @@ print '<ol id="tem_list">';
 my $i = $from;
 foreach (@tem) {
   my $d = tem_descr($_);
-  ## numerazione a mano per non ripartire sempre da 0
+  ## numerazione a mano per non ripartire sempre da 1
   print "<li> $i. <a href=\"/cgi-bin/tematica.pl?ref=$_\"> $_ </a> <p> $d </p> </li>";
   $i++;
 }
 
 print '</ol>';
 
-if ($from > 0) {
+if ($from > 1) {
   print '<a id="a_prec" href="/cgi-bin/mostra_tematiche.pl?from=', $from-10, 
     '" accesskey="p"> &lt; Precedenti </a>';
 }
